1f971a73ec8766eaba454b0585ff37a7637f9775
Brian Roberts <brian@flicken.net>
2020-03-22 10:38:22 +0100

    Externalize config into .env files

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const
+  GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY,
+  GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID;
