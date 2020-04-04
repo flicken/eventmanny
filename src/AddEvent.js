2df68eb15a1df12175a32c69ff3416b99b33a1f3
Brian Roberts <flicken-github@flicken.net>
2020-04-04 08:11:12 +0200

    Focus on summary text again after creating new event

--- src/AddEvent.js
+++ src/AddEvent.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { useForm } from 'react-hook-form'
 
 export default function AddEvent(props) {
+  var summaryInput = null
   const { register, handleSubmit, watch, errors } = useForm()
 
   return (
     <form onSubmit={handleSubmit(props.onSubmit)}>
-      <input type="text" name="summary" ref={register({ required: true })} size="40"/>
+      <input type="text" name="summary" ref={elem => {summaryInput = elem; register({ required: true })(elem)}}  size="40"/>
       <input name="datetimes" ref={register({ required: true })} placeholder="e.g. Monday, 11-12"/>
-      <input name="submitted" type="hidden" value={false}/>
-      <div tabindex="0" onFocus={handleSubmit(props.onSubmit)}/>
+      <div tabIndex="0" onFocus={(a) => {handleSubmit(props.onSubmit)(a); summaryInput.focus(); summaryInput.select()}}/>
     </form>
   )
 
