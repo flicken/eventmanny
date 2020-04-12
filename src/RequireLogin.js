44e74835e33dfa265b0008f39d800df7199f1f25
Brian Roberts <flicken-github@flicken.net>
2020-04-12 09:04:08 +0200

    Reduce scopes needed.
    Don't need to write/delete calendars (yet).

--- src/RequireLogin.js
+++ src/RequireLogin.js
@@ -4,7 +4,11 @@ import { GOOGLE_CLIENT_ID } from "./config.js";
 import { GoogleLogin } from 'react-google-login';
 
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-const SCOPES = "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.readonly profile email"
+const SCOPES =
+  "https://www.googleapis.com/auth/calendar.readonly " +
+  "https://www.googleapis.com/auth/calendar.events.readonly " +
+  "https://www.googleapis.com/auth/calendar.events " +
+  "profile email"
 
 export default function ({isSignedIn, children, onSuccess, onFailure}) {
 
