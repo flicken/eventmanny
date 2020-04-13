f28c27e705fd12c5e56388d6b3dcb16e88f0a972
Brian Roberts <flicken-github@flicken.net>
2020-04-13 13:57:21 +0200

    Fix dateTime capitalization

--- src/RelativeDate.js
+++ src/RelativeDate.js
@@ -4,5 +4,5 @@ import React from 'react';
 import {DateTime} from "luxon"
 
 export default function RelativeDate({date}) {
-  return <time datetime={date.toISO()} title={date.toLocaleString(DateTime.DATETIME_FULL)}>{date.toRelative()}</time>
+  return <time dateTime={date.toISO()} title={date.toLocaleString(DateTime.DATETIME_FULL)}>{date.toRelative()}</time>
 }
