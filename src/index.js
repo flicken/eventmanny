77c7d377a2d87826c08dd029b8416deacfe9cbd4
Brian Roberts <flicken-github@flicken.net>
2020-04-30 15:48:20 +0200

    Remove console log of store

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 
 import store from './redux/store'
-console.log(store)
 
 ReactDOM.render(
   <Provider store={store}>
